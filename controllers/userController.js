f1c87d8173d24ec11ce5f947b932124701dd1072
tabii <tabibito2k512@gmail.com>
2025-10-21 10:39:01 +0700

    sửa lại đường dẫn

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/User");
+const { User } = require("../models/UserModel");
 const generateToken = require("../utils/generateToken");
 
 const createUser = async (req, res) => {
