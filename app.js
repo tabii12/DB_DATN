a355a8ea2f263c54a4e7350b230f39f0bbad957b
tabii <tabibito2k512@gmail.com>
2025-10-23 22:35:13 +0700

    fix app.js

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+  console.error("🔥 Lỗi hệ thống:", err);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Lỗi server nội bộ!",
+  });
 });
 
 module.exports = app;
