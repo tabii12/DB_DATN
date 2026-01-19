bd188ce41b10db19eb849c8f1752b64bc166b580
tabii <tabibito2k512@gmail.com>
2026-01-19 11:20:42 +0700

    fix review route

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const reviewController = require('../controllers/review.controller');
 
 router.post('/create', reviewController.createReview);
-router.get('/tour/:tour_id', reviewController.getReviewsByTour);
+router.get('/tour/:tour_id', reviewController.getAllReviewsByTourId);
 router.put('/:id', reviewController.updateReviewById);
 router.delete('/:id', reviewController.deleteReviewById);
 
