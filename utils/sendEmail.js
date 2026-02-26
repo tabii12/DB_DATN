af359de08f4bdd46c2c97a4a6bda1d2d483c6720
tabii <tabibito2k512@gmail.com>
2026-02-26 15:17:14 +0700

    Replace nodemailer with Resend for email service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,19 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, // app password
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"Pick Your Way" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  try {
+    await resend.emails.send({
+      from: "Pick Your Way <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
+    });
+  } catch (error) {
+    console.error("Resend Error:", error);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
