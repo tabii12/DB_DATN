0698b8db5fb6a3f62a303871c0fef6420b5e1646
tabii <tabibito2k512@gmail.com>
2026-02-27 09:10:27 +0700

    fix: resend not sending email when create new account

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,12 +4,14 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    await resend.emails.send({
+    const response = await resend.emails.send({
       from: "Pick Your Way <onboarding@resend.dev>",
       to,
       subject,
       html,
     });
+
+    console.log("📩 RESEND RESPONSE:", response);
   } catch (error) {
     console.error("Resend Error:", error);
     throw error;
