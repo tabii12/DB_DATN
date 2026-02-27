57fbde99648c5f8b19930e5e9c3514422cf6165e
tabii <tabibito2k512@gmail.com>
2026-02-27 14:55:23 +0700

    fix: redirect multer

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home.controller");
-const upload = require("../middlewares/upload");
+const upload = require("../middlewares/multer");
 
 // ===== HOME =====
 router.get("/", homeController.getHome);
