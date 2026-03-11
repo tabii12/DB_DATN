357476d5dd88811af0b083ffb34dd810381956dc
tabii <tabibito2k512@gmail.com>
2026-03-11 19:03:51 +0700

    fix: upload image in blog create

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const upload = require("../middlewares/multer");
 const blogController = require("../controllers/blog.controller");
 
-router.post("/create", blogController.createBlog);
+router.post("/create", upload.array("images", 5), blogController.createBlog);
 
 router.get("/", blogController.getAllBlogs);
 
