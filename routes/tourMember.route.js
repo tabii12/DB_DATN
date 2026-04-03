ab606431d2549ca47de58e6135858298d285fc0d
tabii <tabibito2k512@gmail.com>
2026-04-03 12:16:09 +0700

    fix: tour member router

--- routes/tourMember.route.js
+++ routes/tourMember.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const TourMemberController = require('../controllers/tourMember.controller');
 
-router.post('/create', TourMemberController.createTourMember);
 router.get('/', TourMemberController.getAllTourMembers);
 router.get('/tour/:tour_id', TourMemberController.getAllTourMembersByTripId);
 router.put('/:id', TourMemberController.updateTourMemberById);
