d83ea82ef9d871dc9ae9a10f3e504a5d276eaf61
tabii <tabibito2k512@gmail.com>
2026-04-04 15:11:54 +0700

    remove

--- app.js
+++ app.js
@@ -35,15 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  cors({
-    origin: "https://datn-sage.vercel.app",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  }),
-);
-
-app.options("*", cors());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/users", userRouter);
