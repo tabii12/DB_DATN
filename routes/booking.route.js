9c4246ba6ad021ba4ea3951a2342723c08f4f976
tabii <tabibito2k512@gmail.com>
2026-04-10 14:09:18 +0700

    fix: NotFoundError

--- routes/booking.route.js
+++ routes/booking.route.js
@@ -5,17 +5,18 @@ const bookingController = require("../controllers/booking.controller");
 const isAdmin = require("../middlewares/isAdmin.middleware");
 const { primaryAuth } = require("../middlewares/auth.middleware");
 
-// Auth routes
-router.post("/", primaryAuth, bookingController.createBooking);
-router.get("/my-bookings", primaryAuth, bookingController.getMyBookings);
-router.get("/:id", primaryAuth, bookingController.getBookingDetail);
-router.patch("/:id/cancel", primaryAuth, bookingController.cancelBooking);
-
 // Admin routes
 router.get("/admin/all", primaryAuth, isAdmin, bookingController.getAllBookings);
 router.patch("/admin/:id/confirm-payment", primaryAuth, isAdmin, bookingController.confirmPayment);
 
-// VNPAY callback (public - from VNPAY redirect)
+// VNPAY callback
 router.get("/vnpay/callback", bookingController.updateVNPayPayment);
+
+// User routes
+router.post("/", primaryAuth, bookingController.createBooking);
+router.get("/my-bookings", primaryAuth, bookingController.getMyBookings);
+
+router.get("/:id", primaryAuth, bookingController.getBookingDetail);
+router.patch("/:id/cancel", primaryAuth, bookingController.cancelBooking);
 
 module.exports = router;
